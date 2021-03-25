a65e5cb272f5702086e6ada9e792a1b6e7861f36
Philip Lowe <philip.lowe94@gmail.com>
2021-03-25 15:27:22 -0400

    Phase 1 of URL Shortener

--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -10,27 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710184251) do
+ActiveRecord::Schema.define(version: 2021_03_25_190805) do
 
-  create_table "courses", force: :cascade do |t|
-    t.string "name"
-    t.integer "prereq_id"
-    t.integer "instructor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "enrollments", force: :cascade do |t|
-    t.integer "course_id"
-    t.integer "student_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
